553ea3ee00e1b7d0b673bfbe21fd733d5a5b8e57
Makan Haghighi <70175993+MakanHaghighi19@users.noreply.github.com>
2024-07-08 08:54:37 -0700

    Update Rover_Receiver.c

--- Rover_Receiver.c
+++ Rover_Receiver.c
@@ -83,6 +83,66 @@ void setup() {
 }
 
 void loop() {
-string dataString;
-if (btSerial.available());
+  String dataString;
+  if (btSerial.available()) {
+    dataString = btSerial.readStringUntil('\n');
+    sscanf(dataString.c_str(), "%d,%d,%d,%d", &data.lxAxisValue, &data.lyAxisValue, &data.rxAxisValue, &data.ryAxisValue);
+
+    int throttle = map(data.lyAxisValue, 254, 0, -255, 255); // Left stick - y axis - forward/backward car movement
+    int steering = map(data.rxAxisValue, 0, 254, -255, 255); // Right stick - x axis - left/right car movement
+
+
+
+    int rightMotorSpeed = 0, leftMotorSpeed = 0;
+
+    if (throttle >= 0) {
+      rightMotorSpeed = throttle - steering;
+      leftMotorSpeed = throttle + steering;
+    } else {
+      rightMotorSpeed = throttle + steering;
+      leftMotorSpeed = throttle - steering;
+    }
+
+    rightMotorSpeed = constrain(rightMotorSpeed, -250, 250);
+    leftMotorSpeed = constrain(leftMotorSpeed, -250, 250);
+
+    int rightMotorDirection = (rightMotorSpeed >= 0) ? 1 : -1;
+    int leftMotorDirection = (leftMotorSpeed >= 0) ? 1 : -1;
+
+    // Control right motor
+    if (rightMotorSpeed > 0) {
+      digitalWrite(rightMotorPin1, HIGH);
+      digitalWrite(rightMotorPin2, LOW);
+    } else if (rightMotorSpeed < 0) {
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, HIGH);
+    } else {
+      digitalWrite(rightMotorPin1, LOW);
+      digitalWrite(rightMotorPin2, LOW);
+    }
+
+    // Control left motor
+    if (leftMotorSpeed > 0) {
+      digitalWrite(leftMotorPin1, HIGH);
+      digitalWrite(leftMotorPin2, LOW);
+    } else if (leftMotorSpeed < 0) {
+      digitalWrite(leftMotorPin1, LOW);
+      digitalWrite(leftMotorPin2, HIGH);
+    } else {
+      digitalWrite(leftMotorPin1, LOW);
+      digitalWrite(leftMotorPin2, LOW);
+    }
+
+    analogWrite(enableRightMotor, abs(rightMotorSpeed));
+    analogWrite(enableLeftMotor, abs(leftMotorSpeed));
+
+    lastRecvTime = millis();
+  } else {
+    unsigned long now = millis();
+    if (now - lastRecvTime > 1000) {
+      // Signal lost after 1 second. Stop the motors
+      analogWrite(enableRightMotor, 0);
+      analogWrite(enableLeftMotor, 0);
+    }
+  }
 }
