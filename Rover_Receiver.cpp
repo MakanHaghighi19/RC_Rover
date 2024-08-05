7d35c00eb0319da25a6ecf8f613ee8b347d6b639
Makan Haghighi <70175993+MakanHaghighi19@users.noreply.github.com>
2024-08-05 08:15:48 -0700

    Complete Rover_Receiver.cpp
    Final version of the receiver code for this personal project.

--- Rover_Receiver.cpp
+++ Rover_Receiver.cpp
@@ -1,126 +1,112 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial btSerial(2, 3); // RX | TX
+SoftwareSerial btSerial(2, 3);
 
 struct PacketData {
-  byte lxAxisValue;
-  byte lyAxisValue;
-  byte rxAxisValue;
-  byte ryAxisValue;
+  byte forwardBackwardValue;
+  byte turnValue;
 };
 
 PacketData data;
 
-// Assign ports for right motor
-int enb = 7;
-int rmotor_pin1 = 10;
-int rmotor_pin2 = 9;
+int rightspeedPin = 7;
+int dirPin3 = 10;
+int dirPin4 = 9;
 
-// Assign ports for left motor
-int ena = 8;
-int lmotor_pin1 = 12;
-int lmotor_pin2 = 11;
+int leftspeedPin = 8;
+int dirPin1 = 12;
+int dirPin2 = 11;
 
-// Assign ports for Ultrasonic Sensor
-int trig_pin = 4;
-int echo_pin = 5;
-
-unsigned long lastRecvTime = 0;
-
-long DistanceMeasurement() {
-  digitalWrite(trig_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig_pin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig_pin, LOW);
-
-  long duration = pulseIn(echo_pin, HIGH);
-  long distance = (duration / 2) / 29.1; // Convert duration to distance
-  return distance; // Return the calculated distance
-}
+int trigPin = 4;
+int echoPin = 5;
 
 void setup() {
-  pinMode(enb, OUTPUT);
-  pinMode(rmotor_pin1, OUTPUT);
-  pinMode(rmotor_pin2, OUTPUT);
+  pinMode(rightspeedPin, OUTPUT);
+  pinMode(dirPin3, OUTPUT);
+  pinMode(dirPin4, OUTPUT);
 
-  pinMode(ena, OUTPUT);
-  pinMode(lmotor_pin1, OUTPUT);
-  pinMode(lmotor_pin2, OUTPUT);
+  pinMode(leftspeedPin, OUTPUT);
+  pinMode(dirPin1, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
 
-  pinMode(trig_pin, OUTPUT);
-  pinMode(echo_pin, INPUT); // Set echo pin as INPUT
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   Serial.begin(9600);
   btSerial.begin(38400);
 }
 
 void loop() {
-  String dataString;
-  long distance = DistanceMeasurement();
+  long distance = measureDistance();
+  if (distance < 35) {
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, 0, true);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, 0, true);
+    return;
+  }
 
-  if (distance < 20) { // Threshold distance for crash detection (20 cm)
-    analogWrite(enb, 0);
-    analogWrite(ena, 0);
+  if (btSerial.available()) {
+    String dataString = btSerial.readStringUntil('\n');
+    sscanf(dataString.c_str(), "%hhu,%hhu", &data.forwardBackwardValue, &data.turnValue);
+    controlMotors(data.turnValue, data.forwardBackwardValue);
+  }
+}
+
+long measureDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = (duration / 2) / 29.1;
+  return distance;
+}
+
+void controlMotors(int forwardBackwardValue, int turnValue) {
+  int forwardSpeed = 0;
+  int turnSpeed = 0;
+
+  if (forwardBackwardValue < 127) {
+    forwardSpeed = map(forwardBackwardValue, 0, 127, 255, 0);
+  } else if (forwardBackwardValue > 127) {
+    forwardSpeed = map(forwardBackwardValue, 127, 255, 0, 255);
+  }
+
+  if (turnValue > 127) {
+    turnSpeed = map(turnValue, 0, 127, 255, 0);
+  } else if (turnValue < 127) {
+    turnSpeed = map(turnValue, 127, 255, 0, 255);
+  }
+
+  if (forwardBackwardValue < 127) {
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, forwardSpeed, true);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, forwardSpeed, true);
+  } else if (forwardBackwardValue > 127) {
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, forwardSpeed, false);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, forwardSpeed, false);
   } else {
-    if (btSerial.available()) {
-      dataString = btSerial.readStringUntil('\n');
-      sscanf(dataString.c_str(), "%d,%d,%d,%d", &data.lxAxisValue, &data.lyAxisValue, &data.rxAxisValue, &data.ryAxisValue);
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, 0, true);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, 0, true);
+  }
 
-      int throttle = map(data.lyAxisValue, 254, 0, -255, 255); // Left stick - y axis - forward/backward car movement
-      int steering = map(data.rxAxisValue, 0, 254, -255, 255); // Right stick - x axis - left/right car movement
+  if (turnValue > 127) {
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, turnSpeed, false);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, turnSpeed, true);
+  } else if (turnValue < 127) {
+    setMotorSpeedAndDirection(rightspeedPin, dirPin3, dirPin4, turnSpeed, true);
+    setMotorSpeedAndDirection(leftspeedPin, dirPin1, dirPin2, turnSpeed, false);
+  }
+}
 
-      int rightMotorSpeed = 0, leftMotorSpeed = 0;
-
-      if (throttle >= 0) {
-        rightMotorSpeed = throttle - steering;
-        leftMotorSpeed = throttle + steering;
-      } else {
-        rightMotorSpeed = throttle + steering;
-        leftMotorSpeed = throttle - steering;
-      }
-
-      rightMotorSpeed = constrain(rightMotorSpeed, -250, 250);
-      leftMotorSpeed = constrain(leftMotorSpeed, -250, 250);
-
-      int rightMotorDirection = (rightMotorSpeed >= 0) ? 1 : -1;
-      int leftMotorDirection = (leftMotorSpeed >= 0) ? 1 : -1;
-
-      // Control right motor
-      if (rightMotorSpeed > 0) {
-        digitalWrite(rmotor_pin1, HIGH);
-        digitalWrite(rmotor_pin2, LOW);
-      } else if (rightMotorSpeed < 0) {
-        digitalWrite(rmotor_pin1, LOW);
-        digitalWrite(rmotor_pin2, HIGH);
-      } else {
-        digitalWrite(rmotor_pin1, LOW);
-        digitalWrite(rmotor_pin2, LOW);
-      }
-
-      // Control left motor
-      if (leftMotorSpeed > 0) {
-        digitalWrite(lmotor_pin1, HIGH);
-        digitalWrite(lmotor_pin2, LOW);
-      } else if (leftMotorSpeed < 0) {
-        digitalWrite(lmotor_pin1, LOW);
-        digitalWrite(lmotor_pin2, HIGH);
-      } else {
-        digitalWrite(lmotor_pin1, LOW);
-        digitalWrite(lmotor_pin2, LOW);
-      }
-
-      analogWrite(enb, abs(rightMotorSpeed));
-      analogWrite(ena, abs(leftMotorSpeed));
-
-      lastRecvTime = millis();
-    } else {
-      unsigned long now = millis();
-      if (now - lastRecvTime > 1000) {
-        // Signal lost after 1 second. Stop the motors
-        analogWrite(enb, 0);
-        analogWrite(ena, 0);
-      }
-    }
+void setMotorSpeedAndDirection(int speedPin, int dirPin1, int dirPin2, int speed, bool forward) {
+  if (speed != 0) {
+    analogWrite(speedPin, abs(speed));
+    digitalWrite(dirPin1, forward ? LOW : HIGH);
+    digitalWrite(dirPin2, forward ? HIGH : LOW);
+  } else {
+    analogWrite(speedPin, 0);
+    digitalWrite(dirPin1, LOW);
+    digitalWrite(dirPin2, LOW);
   }
 }
